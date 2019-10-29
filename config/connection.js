2accdd45cfe9106b3102522cb2e62a1b248ba635
Lucio Verane <verane64@gmail.com>
2019-10-29 13:36:12 -0500

    fixed for heroku

--- config/connection.js
+++ config/connection.js
@@ -8,9 +8,8 @@ if (process.env.JAWSDB_URL) {
 } else {
   connection = mysql.createConnection({
     host: "localhost",
-    port: 3306,
     user: "root",
-    password: "root",
+    password: "",
     database: "burgers_db"
   });
 }
